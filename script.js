5e57f49df0b21ee14e10b7c76dd1c5ff5cff21de
jmd <jordan.michael.dickey@gmail.com>
2021-06-27 14:22:09 -0600

    Added an event handler function for the user input field to assign the value of the input to a variable and allow for comparison with the randomly generated number

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log(document.querySelector('.message'))
+const checkButton = document.querySelector('.check');
+const number = Math.trunc(Math.random()*20)+1
+console.log(number);
+
+checkButton.addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number was entered!⛔';
+  }
+});
