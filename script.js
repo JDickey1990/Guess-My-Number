6a028606e08585bebda63408e269a6a15f046c3b
jmd <jordan.michael.dickey@gmail.com>
2021-07-08 20:12:22 -0600

    implimenting basic game logic

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const checkButton = document.querySelector('.check');
-const number = Math.trunc(Math.random()*20)+1
-console.log(number);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
 
 checkButton.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -10,5 +12,29 @@ checkButton.addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No number was entered!⛔';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = ` 🙌 Correct Number!`;
+  } else if (guess > secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = `Your guess is Too High! 📈 `;
+      score--;
+      document.querySelector('.score').textContent = score;
+    }else {
+      document.querySelector(".message").textContent = `You lost the game!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    }  
+  }else if (guess < secretNumber) {
+    if(score > 1){
+      document.querySelector('.message').textContent = `Your guess is Too Low! 📉 `;
+    score--;
+    document.querySelector('.score').textContent = score;
+    }else {
+      document.querySelector(".message").textContent = `💣 You lost the game!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    }  
+   
   }
+    
 });
