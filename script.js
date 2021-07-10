85c1876ddd4a414202b252ca18e145328a49d5ea
jmd <jordan.michael.dickey@gmail.com>
2021-07-09 20:34:36 -0600

    Adding in game reset functionality

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const checkButton = document.querySelector('.check');
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+
 
 checkButton.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -17,6 +17,7 @@ checkButton.addEventListener('click', function () {
 // When a player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = ` 🙌 Correct Number!`;
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = "#60b347";
     document.querySelector('.number').style.width = '30rem';
 
@@ -46,3 +47,15 @@ checkButton.addEventListener('click', function () {
   }
     
 });
+
+const againButton = document.querySelector(".again");
+againButton.addEventListener('click', function(){
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = `Start guessing...`;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = `?`;
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = "#222";
+  document.querySelector('.number').style.width = '15rem';
+})
